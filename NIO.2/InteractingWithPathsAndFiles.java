8654a7e61bfb0014f9300ea2f1f2d6d31b79e390
Zhousenye Liu <jooppp@live.com>
2016-12-01 20:53:24 -0500

    Update InteractingWithPathsAndFiles.java

--- NIO.2/InteractingWithPathsAndFiles.java
+++ NIO.2/InteractingWithPathsAndFiles.java
@@ -1,1 +1,16 @@
+package test;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class InteractingWithPathsAndFiles{
+	public static void main(String[] args){
+	//Path interface has three methods toString(), getNameCount() and getName() , which allow us get information about path
+	Path path1= Paths.get("/Users/kira/Documents/workspace/NIO.2");
+	System.out.println("the path name:"+path1);
+	for(int i=0;i<path1.getNameCount();i++){  // this method provide number of components of this path 
+		System.out.println("component"+i+": "+path1.getName(i)); 
+	}
+	}
+	
+}
