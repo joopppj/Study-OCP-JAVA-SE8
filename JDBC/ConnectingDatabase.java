53f7283c3acf661437a68f8f15768e4d763e483d
Zhousenye Liu <jooppp@live.com>
2016-12-20 23:41:00 -0500

    Update ConnectingDatabase.java

--- JDBC/ConnectingDatabase.java
+++ JDBC/ConnectingDatabase.java
@@ -1,1 +1,12 @@
+package test;
 
+public class ConnectingDatabase{
+	
+	// a standard JDBC url has three parts:
+	//1 the protocol jdbc (this one is always same) 
+	//2 the name of database product (for example , it can be mysql, postgresql and derby which is inside the JDK)
+	//3 the rest of it, which contains the location ( it is possibly omitted )and the name of the database.
+	//  good url example can be:jdbc:derby:zoo, jdbc:postgresql://localhost/zoo , jdbc:oracle:thin@123.123.123.123:1521:zoo
+	
+
+}
