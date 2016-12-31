fec9d041755f215f3d7825dfd38eed6deb8636e7
jooppp <jooppp@live.com>
2016-12-31 16:50:47 -0500

    Delete EnumsAndNestClass.java

--- Advanced Class Design/EnumsAndNestClass.java
+++
@@ -1,7 +0,0 @@
-public enum EnumsAndNestClass{
-	WINTER,SPRING,SUMMER,FALL;
-	public static void main(String[] args){
-		for(EnumsAndNestClass i:EnumsAndNestClass.values()) //value() return list of all values 
-		System.out.println(i.name()+""+i.ordinal());  // name() and ordinal() return enums'names and their indexes
-	}
-}
