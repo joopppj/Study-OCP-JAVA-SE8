f693788ffd7ecf8e505eee155f835e5b32801b43
jooppp <jooppp@live.com>
2017-01-02 20:41:38 -0500

    Create FunctionalInterface.java

---
+++ DesignPatternsAndPrinciples/FunctionalInterface.java
@@ -0,0 +1,9 @@
+package test;
+
+// any interface that has exactly one abstract method is functional interface.
+public interface FunctionalInterface{
+	public abstract void sprint(String animal);
+}
+/*public class normalClass {
+	
+}*/
