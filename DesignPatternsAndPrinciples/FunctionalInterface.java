0a2fc0ce80eaa844fb6ef79504c785555fa6a7a0
jooppp <jooppp@live.com>
2017-01-06 10:29:22 -0500

    Delete FunctionalInterface.java

--- DesignPatternsAndPrinciples/FunctionalInterface.java
+++
@@ -1,9 +0,0 @@
-package test;
-@FunctionalInterface // if the interface is not functional interface ,this annotation will detect an error
-// any interface that has exactly one abstract method is functional interface.
-public interface AnyInterface{
-	public abstract void sprint(String animal);
-}
-/*public class normalClass {
-	
-}*/
