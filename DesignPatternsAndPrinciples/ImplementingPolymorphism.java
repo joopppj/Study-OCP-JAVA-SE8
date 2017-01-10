9f4e42428846e35774bd8095fbb8efe6201812a7
jooppp <jooppp@live.com>
2017-01-09 23:16:14 -0500

    Update ImplementingPolymorphism.java

--- DesignPatternsAndPrinciples/ImplementingPolymorphism.java
+++ DesignPatternsAndPrinciples/ImplementingPolymorphism.java
@@ -1,2 +1,16 @@
+package test;
+//Polymorphism is the ability of a single interface to support multiple underlying forms
+interface human{
+	public String speak();
+}
+class idiot implements human{
+	public String speak(){return "";}
+}
+class talent implements human{
+	public String speak(){return "";}
+}
 public class ImplementingPolymorphism{
-  }
+	public static void main(String[] args){
+		human idiot = new idiot();  // polymorphism allows one object to be referenced by the interface it implements
+	}
+}
