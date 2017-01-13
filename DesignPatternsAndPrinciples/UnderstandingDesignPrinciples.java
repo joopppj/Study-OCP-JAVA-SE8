fb35160f2a3e312fb46d264f21732378cf673fc1
jooppp <jooppp@live.com>
2017-01-12 22:14:38 -0500

    Update UnderstandingDesignPrinciples.java

--- DesignPatternsAndPrinciples/UnderstandingDesignPrinciples.java
+++ DesignPatternsAndPrinciples/UnderstandingDesignPrinciples.java
@@ -3,12 +3,34 @@ package test;
 // encapsulation is to protect fields/methods from using by other users , int technical words: to set those variables/methods private
 // if we have a class without encapsulation, its variable will easily be changed from outside even they should not
 class Animal{
-	public int age; 
+	public int wrongAge; 
+	private int age;
+	private boolean mammal;
+	private Boolean pet;
+	public boolean isMammal() {
+		return mammal;
+	}
+	public void setMammal(boolean mammal) {
+		this.mammal = mammal;
+	}
+	public Boolean isPet() {   //Boolean getter cannot start with is since it is not boolean
+		return pet;
+	}
+	public void setPet(Boolean pet) {  //setters begin with set
+		this.pet = pet;
+	}
+	public int getAge() {  //getters begin withget
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
 
 public class UnderstandingDesignPrinciples{
 	public static void main(String[] args){
 		Animal animal= new Animal();
-		animal.age=-1100;     
+		animal.wrongAge=-1100;   
+		//animal.age= -100;// so we need a encapsulation to protect important variable from being changed.
 	}
 }
