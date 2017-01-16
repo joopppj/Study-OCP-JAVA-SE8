6d38691990f854a53a0606389eb5eac859958105
jooppp <jooppp@live.com>
2017-01-15 23:58:46 -0500

    Update workingWithGenerics.java

--- GenericsAndCollection/workingWithGenerics.java
+++ GenericsAndCollection/workingWithGenerics.java
@@ -1,1 +1,10 @@
-
+package test;
+public class workingWithGenerics<T>{
+	private T contents;
+	public T emptyCrate(){
+		return contents;
+	}
+	public void packCrate(T contents){
+		this.contents=contents;
+	}
+}
